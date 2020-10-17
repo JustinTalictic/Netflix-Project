f00879bb1b1f0a5092cccfff795401ecf1a94058
Justin Talictic <jaytalictic@gmail.com>
2020-10-17 13:03:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
