8567667540fd976c98c164c7825119aa861f07fb
Justin Talictic <jaytalictic@gmail.com>
2020-10-25 17:11:53 -0500

    browse feature/search bar implemented

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -6,7 +6,10 @@ import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
 
 export function BrowseContainer() {
+    const [category, setCategory] = useState('series');
     const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const user = {
         displayName: 'Karl',
@@ -15,6 +18,49 @@ export function BrowseContainer() {
 
     return profile.displayName ? (
         <>
+            <Header src="joker1" dontShowOnSmallViewPort>
+                <Header.Frame>
+                    <Header.Group>
+                        <Header.Logo
+                            to={ROUTES.HOME}
+                            src="/images/misc/logo.svg"
+                            alt="Netflix"
+                        />
+                        <Header.Link
+                            active={category === 'series' ? 'true' : 'false'}
+                            onClick={() => setCategory('series')}
+                        >
+                            Series
+                        </Header.Link>
+                        <Header.Link
+                            active={category === 'films' ? 'true' : 'false'}
+                            onClick={() => setCategory('films')}
+                        >
+                            Films
+                        </Header.Link>
+                    </Header.Group>
+                    <Header.Group>
+                        <Header.Search
+                            searchTerm={searchTerm}
+                            setSearchTerm={setSearchTerm}
+                        />
+                    </Header.Group>
+                </Header.Frame>
+
+                <Header.Feature>
+                    <Header.FeatureCallOut>
+                        Watch Joker Now
+                    </Header.FeatureCallOut>
+                    <Header.Text>
+                        Forever alone in a crowd, failed comedian Arthur Fleck
+                        seeks connection as he walks the streets of Gotham City.
+                        Arthur wears two masks -- the one he paints for his day
+                        job as a clown, and the guise he projects in a futile
+                        attempt to feel like he's part of the world around him.
+                    </Header.Text>
+                    <Header.PlayButton>Play</Header.PlayButton>
+                </Header.Feature>
+            </Header>
             <p>Browse Container</p>
             <FooterContainer />
         </>
