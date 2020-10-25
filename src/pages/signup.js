f8707f2811ed2ebbcea01c65217e91f2bab01ff6
Justin Talictic <jaytalictic@gmail.com>
2020-10-24 19:29:15 -0500

    Sign up/sign in added

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import { Form } from '../components';
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
+import * as ROUTES from '../';
 
-export default function Signin() {
-    const [error, setError] = useState('');
+export default function Signup() {
+    const [firstName, setFirstName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-    const isInvalid = (password === '') | (emailAddress === '');
+    const isInvalid =
+        password === '' || emailAddress === '' || firstName === '';
 
-    const handleSignin = e => {
+    const handleSignup = e => {
         e.preventDefault();
     };
 
@@ -18,10 +21,17 @@ export default function Signin() {
         <>
             <HeaderContainer>
                 <Form>
-                    <Form.Title>Sign In</Form.Title>
+                    <Form.Title>Sign Up</Form.Title>
                     {error && <Form.Error>{error}</Form.Error>}
 
-                    <Form.Base onSubmit={handleSignin} method="POST">
+                    <Form.Base onSubmit={handleSignup} method="POST">
+                        <Form.Input
+                            placeholder="First Name"
+                            value={firstName}
+                            onChange={({ target }) =>
+                                setFirstName(target.value)
+                            }
+                        />
                         <Form.Input
                             placeholder="Email address"
                             value={emailAddress}
@@ -37,12 +47,12 @@ export default function Signin() {
                             onChange={({ target }) => setPassword(target.value)}
                         />
                         <Form.Submit disabled={isInvalid} type="submit">
-                            Sign In
+                            Sign Up
                         </Form.Submit>
 
                         <Form.Text>
-                            New to Netflix?{' '}
-                            <Form.Link to="/signup">Sign up now.</Form.Link>
+                            Already a user?{' '}
+                            <Form.Link to="/signin">Sign up now.</Form.Link>
                         </Form.Text>
                         <Form.TextSmall>
                             This page is protected by Google reCAPTCHA.
